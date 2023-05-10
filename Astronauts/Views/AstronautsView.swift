70ad345f69d27cfee3259dfedfab91fb22342df6
Md Ashadur Rahman <md.ashadur.rahman@accenture.com>
2023-05-11 01:28:16 +0200

    Astronauts view updated with login condition

--- Astronauts/Views/AstronautsView.swift
+++ Astronauts/Views/AstronautsView.swift
@@ -1,43 +1,61 @@
 
 import SwiftUI
+import FacebookLogin
 
 struct AstronautsView: View {
     @StateObject var astronauts = AstronautsViewModel()
+    @StateObject var manageLogin = SocialLoginManager()
     
     var body: some View {
-        NavigationView {
-            List(astronauts.astronautsArray, id: \.id) {
-                astronaut in
-                NavigationLink(
-                    destination: AstronautDetailsView(astronauts: astronaut),
-                    label: {
-                        HStack {
-                            AsyncImage(url: URL(string: astronaut.profile_image_thumbnail))
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 80, height: 80)
-                                .clipped()
-                                .cornerRadius(40)
+        ZStack {
+            if !manageLogin.logged {
+                SocialLoginView()
+            }
+            else {
+                NavigationView {
+                    List(astronauts.astronautsArray, id: \.id) {
+                        astronaut in
+                        NavigationLink(
+                            destination: AstronautDetailsView(astronauts: astronaut),
+                            label: {
+                                HStack {
+                                    AsyncImage(url: URL(string: astronaut.profile_image_thumbnail))
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 80, height: 80)
+                                        .clipped()
+                                        .cornerRadius(40)
+                                    
+                                    VStack(alignment: .leading) {
+                                        Text(astronaut.name)
+                                            .font(.title2)
+                                        Text("Age: \(astronaut.age)")
+                                            .font(.body)
+                                    }.padding(4)
+                                }
+                            }
+                        )
+                    }
+                    .navigationTitle("Astronauts")
+                    .toolbar{
+                        ToolbarItemGroup(placement: .navigationBarTrailing) {
+                            Button("Logout", action: {
+                                manageLogin.logoutFromUser()
+                            })
                             
-                            VStack(alignment: .leading) {
-                                Text(astronaut.name)
-                                    .font(.title2)
-                                Text("Age: \(astronaut.age)")
-                                    .font(.body)
-                            }.padding(4)
                         }
                     }
-                )
+                    .task {
+                        await astronauts.getAstronauts()
+                    }
+                }
             }
-            .navigationTitle("Astronauts")
-        }
-        .task {
-            await astronauts.getAstronauts()
+            
         }
     }
-}
-
-struct AstronautsView_Previews: PreviewProvider {
-    static var previews: some View {
-        AstronautsView()
+    
+    struct AstronautsView_Previews: PreviewProvider {
+        static var previews: some View {
+            AstronautsView()
+        }
     }
 }
